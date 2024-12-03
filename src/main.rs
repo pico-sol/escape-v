614c92955676b6dbbecaeeb70ffe9420887edec7
pico-sol <qmata55@gmail.com>
2024-12-03 10:43:55 +0000

    debugged

--- src/main.rs
+++ src/main.rs
@@ -33,10 +33,10 @@ async fn main() -> Result<()> {
         // VaultkaのUSDCプールの残高を表示
         let usdc_balance_data = ctx.rpc.get_account_data(&Pubkey::from_str("E19zKjNZhWhvHfHao89Pk9xQ2zSr6DErGSaFnddyuY3A")?).await?;
         let balance = spl_token::state::Account::unpack_from_slice(&usdc_balance_data).unwrap().amount;
-        let balance = if balance < 103562 {
+        let balance = if balance < 100000 {
             0
         } else {
-            balance - 103562
+            balance - 100000
         };
         println!("vaultka balance: {:?} USDC", balance as f64 / 1_000_000_f64);
         let withdraw_amount = min(balance, DEPOSITED_AMOUNT);
